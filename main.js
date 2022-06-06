42c7926afaa3ac05c4b9936fc0d28b347b4f54b3
Advita-Ishi <80190120+Advita-Ishi@users.noreply.github.com>
2022-06-06 12:33:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,31 @@
 function preload() {
-img = loadImage('background.jpg')
+
 }
 
 function setup() {
     canvas = createCanvas(300,300);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPose);
 }
 
+function modelLoaded() {
+    console.log('PoseNet Is Initialized');
+}
+
+function gotPose(results) {
+    if(results.length > 0) {
+        console.log(results);
+        console.log("nose x =" + results[0].pose.nose.x);
+        console.log("nose y =" + results[0].pose.nose.y);
+    }
+}
 function draw() {
-image(img,0,0,300,300);
+image(video,0,0,300,300);
 }
 
 function take_snapshot() {
